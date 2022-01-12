2d2f00ed7ed9e91659fa6c397b1323e8fa418469
NiroshaSegar <niroshas@treinetic.com>
2022-01-12 23:33:50 +0530

    Simple microservice using Spring Boot

---
+++ Microservices/Practical/Demo-Application/src/main/java/com/nirosha/rentbook/demoapplication/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.nirosha.rentbook.demoapplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String greeting(){
+        return "Hello Spring Boot from, GET";
+
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
+    public String greeting2(){
+        return "Hello Spring Boot from, POST";
+
+    }
+}
