1e2113dfead60ab3569789adaba179fc6f37d54d
NiroNisha698 <nironisha0606@gmail.com>
2022-01-13 21:24:14 +0530

    P003- Create DB connection

--- Microservices/Practical/Demo-Application/src/main/java/com/nirosha/rentbook/demoapplication/controller/MainController.java
+++ Microservices/Practical/Demo-Application/src/main/java/com/nirosha/rentbook/demoapplication/controller/MainController.java
@@ -1,11 +1,18 @@
 package com.nirosha.rentbook.demoapplication.controller;
 
+import com.nirosha.rentbook.demoapplication.model.Book;
+import com.nirosha.rentbook.demoapplication.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @Autowired
+    BookService bookService;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String greeting(){
@@ -17,5 +24,10 @@ public class MainController {
     public String greeting2(){
         return "Hello Spring Boot from, POST";
 
+    }
+
+    @RequestMapping(value = "/book" , method = RequestMethod.POST)
+    public Book save(@RequestBody Book book){
+     return bookService.save(book);
     }
 }
